479cccdf0920a62e0d15f02072bed3d086f02745
Vasily Kolosov <v.kolosov@quineex.com>
2016-09-05 17:46:05 +0300

    Better output

--- image_verify.py
+++ image_verify.py
@@ -2,7 +2,16 @@
 # -*- coding: utf-8 -*-
 
 
-"""Checks images for corruption
+"""Checks images for corruption in a given directory recursively.
+
+Dependencies:
+
+    sudo pip install Pillow OpenEXR
+
+Usage:
+
+    python image_verify.py /path/to/root/folder/with/images
+        # ^^^^^^ will check all PNGs, JPGs and EXRs recursively in specified directory
 
 """
 
@@ -13,8 +22,8 @@ import os
 import logging
 logging.basicConfig(
     # filename='image_verify.log',
-    level=logging.WARN,
-    format='%(asctime)s %(message)s'
+    level=logging.INFO,
+    format='%(asctime)20s %(msg_tag)16s  %(message)s'
 )
 
 openexr_enabled = False
@@ -26,6 +35,7 @@ except:
     pass
 
 from multiprocessing import Pool, cpu_count
+from collections import Counter
 
 from PIL import Image
 
@@ -34,27 +44,41 @@ if openexr_enabled:
     IMAGE_EXTS += ['exr', ]
 
 
+stats_counter = Counter()
+
+
 def process_image(image_path):
     try:
         statinfo = os.stat(image_path)
         if statinfo.st_size == 0:
             return
 
-        if image_path.lower().endswith('.exr'):
+        # OpenEXR file check
+        if image_path.lower().endswith('.exr') and openexr_enabled:
             if not OpenEXR.isOpenExrFile(image_path):
-                logging.warning('%s: OpenEXR could not read file', image_path)
+                logging.warning('%s: OpenEXR could not read file', image_path, extra={'msg_tag': "EXR_ERROR"})
+            stats_counter["EXR_ERROR"] += 1
             return
 
-        img = Image.open(image_path)
-        try:
-            _ = img.verify()
-        except Exception as e:
-            logging.warning(image_path)
-            logging.warning('%s: %s', image_path, e)
+        # PIL-supported file formats
+        else:
+            img = Image.open(image_path)
+            try:
+                _ = img.verify()
+            except Exception as e:
+                logging.warning(image_path)
+                logging.warning('%s: %s', image_path, e, extra={'msg_tag': "PIL_ERROR"})
+                stats_counter["PIL_ERROR"] += 1
+                return
 
     except Exception as e:
         logging.warning(image_path)
-        logging.error('%s: %s', image_path, e)
+        logging.error('%s: %s', image_path, e, extra={'msg_tag': "OTHER_ERROR"})
+        stats_counter["OTHER_ERROR"] += 1
+        return
+
+    logging.info(image_path, extra={'msg_tag': 'OK'})
+    stats_counter["OK"] += 1
 
 
 def iterate_images(root_folder):
@@ -84,9 +108,14 @@ def main():
     args = parse_args()
 
     for image_path in iterate_images(args.root_folder):
-        print "Processing: {0}".format(image_path)
+        logging.info(image_path, extra={'msg_tag': 'OPEN_IMAGE'})
         # pool.apply_async(process_image, [image_path, ])
         process_image(image_path)
+
+    print
+    print "Stats:"
+    for k, v in stats_counter.items():
+        print "{}: {}".format(k, v)
 
     return 0
 
