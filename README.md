dd3d6eb32227b9ec264a8ed41f72dee2a3cf9625
Vasily Kolosov <v.kolosov@quineex.com>
2016-11-07 13:33:42 +0300

    Update command line arguments. Add checks for movie files.

--- README.md
+++ README.md
@@ -4,18 +4,23 @@ Check PNG, JPG and EXR files for corruption.
 
 # Dependencies
 
-Linux-based distros: `sudo pip install Pillow OpenEXR`
+Requires ffmpeg for MP4 and MOV checks.
 
-Mac: `pip install Pillow && brew install openexr && sudo pip install openexr`
+Linux-based distros: `sudo pip install Pillow OpenEXR pexpect`, also install ffmpeg if required using yum / apt / whatever.
+
+Mac: `pip install Pillow && brew install openexr ffmpeg && sudo pip install openexr pexpect`
 
 # Usage
 
-`python image_verify.py /path/to/root/folder/with/images` will check all PNGs, JPGs and EXRs recursively in specified directory, printing overall stats when finished.
+`python image_verify.py --enable-images --enable-movies --enable-exr /path/to/root/folder/with/images` will check all PNG, JPG, EXR, MOV and MP4 files recursively in specified directory, printing overall stats when finished.
 
 Files with errors are marked with following tags:
 
 - EXR_ERROR: OpenEXR throwed error when calling `OpenEXR.isOpenExrFile`
 - PIL_ERROR: File did not pass PIL `Image.verify` test
+- FFMPEG_ERROR: File did not pass ffmpeg verification test
 - OTHER_ERROR: I/O or system error (access denied, etc)
 
 Use `--verbose` switch to print files being open and files passing the check.
+
+Use `--log-file /path/to/log/file.log` to log all operations to specified log file instead of stdout.
--- image_verify.py
+++ image_verify.py
@@ -4,15 +4,6 @@
 
 """Checks images for corruption in a given directory recursively.
 
-Dependencies:
-
-    sudo pip install Pillow OpenEXR
-
-Usage:
-
-    python image_verify.py /path/to/root/folder/with/images
-        # ^^^^^^ will check all PNGs, JPGs and EXRs recursively in specified directory
-
 """
 
 
@@ -20,6 +11,13 @@ import sys
 import argparse
 import os
 import logging
+
+PEXPECT_AVAILABLE = False
+try:
+    import pexpect
+    PEXPECT_AVAILABLE = True
+except ImportError:
+    pass
 
 openexr_enabled = False
 try:
@@ -34,36 +32,103 @@ from collections import Counter
 
 from PIL import Image
 
-IMAGE_EXTS = ['png', 'jpg', 'jpeg', ]
-if openexr_enabled:
-    IMAGE_EXTS += ['exr', ]
+
+MASTERS_EXTS = ('mov', 'mp4', )
 
 
 stats_counter = Counter()
 
 
-def process_image(image_path):
+class FileNotFoundEx(Exception):
+    pass
+
+
+def check_exr(path):
+    if not OpenEXR.isOpenExrFile(path):
+        logging.warning("EXR_ERROR %s : OpenEXR could not read file", path)
+        return {'valid': False, 'message': 'OpenEXR could not read file'}
+    return {'valid': True, 'message': 'ok'}
+
+
+def check_pil(path):
     try:
+        img = Image.open(path)
+        _ = img.verify()
+    except Exception as e:
+        logging.warning('PIL_ERROR %s : %s', path, e)
+        return {'valid': False, 'message': '{}'.format(e)}
+
+    return {'valid': True, 'message': 'ok'}
+
+
+def check_movie(path):
+    def shellquote(s):
+        return "'" + s.replace("'", "'\\''") + "'"
+
+    cmd = 'ffmpeg -i {0} -f null -'.format(shellquote(path))
+    thread = pexpect.spawn(cmd)
+
+    patterns = thread.compile_pattern_list(
+        [
+            pexpect.EOF,
+            ".*(damaged|error|invalid|Error|Invalid).*",
+            ".*No such file or directory.*",
+            '(.+)',
+        ]
+    )
+
+    while True:
+        i = thread.expect_list(patterns, timeout=None)
+        if i == 0:
+            # No errors, we're good
+            return {'valid': True, 'message': 'ok'}
+        elif i == 1:
+            # FFMpeg found an error
+            logging.warning('FFMPEG_ERROR %s : %s', path, 'ffmpeg error')
+            return {'valid': False, 'message': 'ffmpeg error'}
+        elif i == 2:
+            # File not found
+            raise FileNotFoundEx("{0}: file not found".format(path))
+        elif i == 3:
+            # File being processed, wait
+            pass
+
+
+def process_image(image_path):
+
+    checks_map = [
+        {
+            'ext': ('exr', ),
+            'func': check_exr,
+            'counter': 'EXR_ERROR',
+        },
+        {
+            'ext': ('png', 'jpg', 'jpeg', ),
+            'func': check_pil,
+            'counter': 'PIL_ERROR',
+        },
+        {
+            'ext': ('mp4', 'mov', ),
+            'func': check_movie,
+            'counter': 'FFMPEG_ERROR',
+        }
+    ]
+
+    try:
+
         statinfo = os.stat(image_path)
         if statinfo.st_size == 0:
             return
 
-        # OpenEXR file check
-        if image_path.lower().endswith('.exr') and openexr_enabled:
-            if not OpenEXR.isOpenExrFile(image_path):
-                logging.warning("EXR_ERROR %s : OpenEXR could not read file", image_path)
-                stats_counter["EXR_ERROR"] += 1
-                return
+        for check in checks_map:
+            for ext in check['ext']:
+                if not image_path.lower().endswith('.{}'.format(ext)):
+                    continue
 
-        # PIL-supported file formats
-        else:
-            try:
-                img = Image.open(image_path)
-                _ = img.verify()
-            except Exception as e:
-                logging.warning('PIL_ERROR %s : %s', image_path, e)
-                stats_counter["PIL_ERROR"] += 1
-                return
+                check_result = check['func'](image_path)
+                if not check_result['valid']:
+                    stats_counter[check['counter']] += 1
+                    return
 
     except Exception as e:
         logging.error('OTHER_ERROR %s: %s', image_path, e)
@@ -74,13 +139,40 @@ def process_image(image_path):
     stats_counter["OK"] += 1
 
 
-def iterate_images(root_folder):
+def iterate_files(root_folder):
     for root, dirs, files in os.walk(root_folder):
         for file_name in files:
-            if not any([file_name.lower().endswith('.' + ext) for ext in IMAGE_EXTS]):
+            if not any([file_name.lower().endswith('.' + ext) for ext in CHECK_EXTS]):
                 continue
 
             yield os.path.join(root, file_name)
+
+
+def check_ffmpeg_installed():
+    """Checks whether ffmpeg is installed on this system.
+
+    Returns:
+        True: ffmpeg is installed
+        False: ffmpeg is not installed
+
+    """
+    thread = pexpect.spawn('ffmpeg')
+    patterns = thread.compile_pattern_list(
+        [
+            pexpect.EOF,
+            '.*(ffmpeg version).+',
+            '(.+)',
+        ]
+    )
+
+    while True:
+        i = thread.expect_list(patterns, timeout=None)
+        if i == 0:
+            raise Exception
+        elif i == 1:
+            return True
+        elif i == 2:
+            return False
 
 
 def parse_args():
@@ -92,6 +184,14 @@ def parse_args():
     )
 
     parser.add_argument('-v', '--verbose', help="Verbose output — show files opening and files that pass the checks", action='store_true')
+
+    parser.add_argument('--enable-movies', help="Include checks for MP4 and MOV files using ffmpeg", action='store_true')
+
+    parser.add_argument('--enable-images', help="Include checks for PNG, JPG and JPEG files using PIL", action='store_true')
+
+    parser.add_argument('--enable-exr', help="Include checks for EXR files using OpenEXR library", action='store_true')
+
+    parser.add_argument('--log-file', help="Log file path, will only log to stdout if not specified")
 
     #   Add arguments below
     parser.add_argument('root_folder', help='Path to the root folder to check.')
@@ -105,13 +205,47 @@ def main():
     if args.verbose:
         log_level = logging.DEBUG
 
-    logging.basicConfig(
-        # filename='image_verify.log',
-        level=log_level,
-        format='%(asctime)20s %(message)s'
-    )
+    if args.log_file:
+        logging.basicConfig(
+            level=log_level,
+            format='%(asctime)20s %(message)s',
+            filename=args.log_file
+        )
+    else:
+        logging.basicConfig(
+            level=log_level,
+            format='%(asctime)20s %(message)s',
+        )
 
-    for image_path in iterate_images(args.root_folder):
+    check_exts = ()
+
+    if args.enable_images:
+        check_exts += ('png', 'jpg', 'jpeg', )
+
+    if args.enable_exr:
+        if not openexr_enabled:
+            logging.error("Please install OpenEXR (pip install openexr) to enable EXR checks")
+            return 5
+        check_exts += ('exr', )
+
+    if args.enable_movies:
+        if not PEXPECT_AVAILABLE:
+            logging.error("Please, install pexpect (pip install pexpect) to enable movie checks with ffmpeg")
+            return 10
+        if not check_ffmpeg_installed():
+            logging.error("Please install ffmpeg to enable movie file checks")
+            return 20
+
+        check_exts += ('mp4', 'mov', )
+
+    global CHECK_EXTS
+    CHECK_EXTS = check_exts
+
+    if not check_exts:
+        logging.error("Please enable one or more file check options, see --help for full list")
+        return 25
+
+    for image_path in iterate_files(args.root_folder):
         logging.info("OPEN_IMAGE %s", image_path)
         # pool.apply_async(process_image, [image_path, ])
         process_image(image_path)
